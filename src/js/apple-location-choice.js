73b4e3ce9639a1e621103c607b960b8fc9a757b0
bg-shorthand <hanhanhan333@gmail.com>
2021-02-08 11:14:49 +0900

    fix: apple location choice

--- src/js/apple-location-choice.js
+++ src/js/apple-location-choice.js
@@ -1,18 +1,18 @@
 import snakeState from './snake-state';
 
 export default () => {
-  let randomX = Math.floor(Math.random() * 40) * 10;
-  let randomY = Math.floor(Math.random() * 40) * 10;
+  let randomX = Math.floor(Math.random() * 39) * 10;
+  let randomY = Math.floor(Math.random() * 39) * 10;
   let check = snakeState.filter(v => v.x === randomX && v.y === randomY);
 
   while (check.length === 1) {
-    randomX = Math.floor(Math.random() * 40) * 10;
-    randomY = Math.floor(Math.random() * 40) * 10;
+    randomX = Math.floor(Math.random() * 39) * 10;
+    randomY = Math.floor(Math.random() * 39) * 10;
     check = snakeState.filter(v => v.x === randomX && v.y === randomY);
   }
 
   return {
-    randomX: Math.floor(Math.random() * 40) * 10,
-    randomY: Math.floor(Math.random() * 40) * 10
+    randomX: Math.floor(Math.random() * 39) * 10,
+    randomY: Math.floor(Math.random() * 39) * 10
   };
 };
